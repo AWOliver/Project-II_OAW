e19ec798183dd372711f67e164f1b154b459b179
Oliver Ager Wiklander <agerwiklander.oliver@elev.ga.ntig.se>
2022-03-09 02:48:10 +0100

    addList js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-let addList = document.querySelector('.lista ul');
+let addList = document.querySelector('.list ul li');
+let pressListBtn = document.querySelector('.listBtn');
+let inputList = document.querySelector('.listTxt')
 
+pressListBtn.addEventListener('click', function(){
+    const createList = document.createElement('LI');
+    const listCont = document.createTextNode(inputList.value);
+
+    createList.appendChild(listCont);
+
+    addList.appendChild(createList);
+});
