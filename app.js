c08c1abe87393c409f5173d923185b2edcdaf63b
Oliver Ager Wiklander <agerwiklander.oliver@elev.ga.ntig.se>
2022-03-16 23:54:35 +0100

    egs

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 let addList = document.querySelector(".listMain");
-let listBtn = document.querySelector(".listBtn");
+let addBtn = document.querySelector(".listBtn");
 let listInput = document.querySelector(".listTxt");
 
-let items = [];
 
-listBtn.addEventListener("click", () => {
-  addItem(listInput.value);
-});
 
-function addItem(text) {
-  items.push(text);
-  updateList();
-  listInput.value = "";
-}
+function itemBtn(e) {
+  e.preventDefault();
 
-function updateList() {
-  addList.innerHTML = "";
-  items.forEach((item) => {
-    let newLi = document.createElement("li");
-    newLi.innerHTML = item;
-    addList.append(newLi);
-  });
-}
-
-var removeItem = document.getElementsByClassName("items");
-var i;
-
-for (i = 0; i < items.length; i++) {
-  items[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+   
 }
