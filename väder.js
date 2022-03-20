ee200e5d471809c01a9fb7597a9cc561bfad88e2
Oliver Ager Wiklander <agerwiklander.oliver@elev.ga.ntig.se>
2022-03-20 01:24:21 +0100

    js

--- väder.js
+++ väder.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
 
-let main = document.querySelector('.weatherAll');
-let temp = document.querySelector('.funäsTemp');
+let weatherMain = document.querySelector('.weatherAll');
+let funäsTemp = document.querySelector('.funäsTemp');
 let funäs = document.querySelector('.funäs');
 
 fetch('https://api.openweathermap.org/data/2.5/weather?lat=62.5419159&lon=12.54845720000003&appid=592624802a3152997ad95fd2435b3068')
@@ -9,7 +9,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?lat=62.5419159&lon=12.548
  .then(data => {
      console.log(data);
      let currentTemp = data.main.temp;
-     temp.textContent = currentTemp.value - 273;
-     console.log(temp)
+     funäsTemp.innerHTML = currentTemp - 273|0;
+     console.log(funäsTemp.innerHTML +'°C');
  });
 });
